fd58bcf147d3d11b37d7b35b170369027eb70d28
pavel <pavelmstu@stego.su>
2018-03-02 15:54:33 +0300

    + default.mlt.json

--- core/md2latex.py
+++ core/md2latex.py
@@ -9,6 +9,7 @@ Module for convert MarkDown to LaTeX
 Created by pavel on 02.03.18 14:03
 """
 import os
+import shutil
 import datetime
 
 from settings import ROOT_PATH
@@ -20,17 +21,26 @@ __author__ = 'pavel'
 
 def convert(
     file_in,
-    file_out
+    file_out,
+    *,
+    file_in_json=None
 ):
     """
     Simple function for convert
-    :param file_in:
-    :param file_out:
+    :param file_in: *.mtl input
+    :param file_out: *.tex output
+    :param file_in_json:
     :return:
     """
-    json_header_path = file_in + '.json'
+    if file_in_json is None:
+        file_in_json = file_in + '.json'
+        if not os.path.exists(file_in_json):
+            shutil.copy(
+                os.path.join(ROOT_PATH, 'core', 'default.mlt.json'),
+                file_in_json,
+            )
 
-    mt = MltTree(json_header_path=json_header_path)
+    mt = MltTree(json_header_path=file_in_json)
 
     with open(file_in, 'r') as fr:
         for line in fr:
